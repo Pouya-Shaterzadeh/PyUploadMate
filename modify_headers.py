148dbffa96c9aa591a3ae6b8b38f83b5af05ee8f
Pouya Shaterzadeh <pouya.shaterzadeh@bahcesehir.edu.tr>
2024-11-27 03:34:03 +0300

    Update modify_headers.py

--- modify_headers.py
+++ modify_headers.py
@@ -4,3 +4,5 @@ def response(flow: http.HTTPFlow) -> None:
     # Modify response headers
     flow.response.headers["Cache-Control"] = "public, max-age=31536000"
     flow.response.headers["X-Content-Type-Options"] = "nosniff"
+
+# Run => mitmdump -s modify_headers.py -p 8081 on command prompt <= to start the proxy server.
